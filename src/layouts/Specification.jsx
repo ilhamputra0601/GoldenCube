759afbff8042a7c759711eb14f5a46d6f85fbc3c
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-03-05 11:33:08 +0700

    flex

--- src/layouts/Specification.jsx
+++ src/layouts/Specification.jsx
@@ -6,7 +6,7 @@ const Specification = () => {
                     <div className="border m-2 border-transparent bg-black/80 fill-none bg-clip-border">
                     <div className="p-10">
                         <h1 className="text-center font-bold bg-golden  bg-clip-text text-transparent text-3xl">Specification</h1>
-                            <div className="mt-5 flex flex-wrap md:flex-nowrap gap-4">
+                            <div className="mt-5 flex flex-col md:flex-row gap-4">
                                 <div className="border border-transparent bg-golden bg-clip-border w-full md:w-1/2">
                                     <div className="border  p-[2px] border-transparent bg-black/80 fill-none bg-clip-border  ">
                                             <img src="https://source.unsplash.com/700x400?briquette" alt="" className=" object-cover overflow-hidden object-center hover:scale-125 transition-all duration-500" />
